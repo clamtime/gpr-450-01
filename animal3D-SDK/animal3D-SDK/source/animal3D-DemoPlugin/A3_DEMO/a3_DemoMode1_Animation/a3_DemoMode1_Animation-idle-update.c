01a67c80ec0922169939918af2dbd186e1f3d792
charlie :) <csanford43@gmail.com>
2021-09-17 11:17:22 -0400

    update comment

--- animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/a3_DemoMode1_Animation/a3_DemoMode1_Animation-idle-update.c
+++ animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/a3_DemoMode1_Animation/a3_DemoMode1_Animation-idle-update.c
@@ -81,6 +81,13 @@ void a3animation_update(a3_DemoState* demoState, a3_DemoMode1_Animation* demoMod
 			activeCamera->projectionMat.m, activeCameraObject->modelMat.m, activeCameraObject->modelMatInv.m,
 			demoMode->object_scene[i].modelMat.m, a3mat4_identity.m);
 	}
+
+
+	// resolve animation: "the 4 steps"
+	//	-> interpolate (between deltas)
+	//	-> concatenate (with base)
+	//	-> convert
+	//	-> FK
 }
 
 
